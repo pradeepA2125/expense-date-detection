813c2ce2708794fe04864a5f277c207e3ece3b07
pradeep kumar <pradeepkumar94p@gmail.com>
2019-12-04 18:44:52 +0530

    Update rekognitionCode.py

--- rekognitionCode.py
+++ rekognitionCode.py
@@ -3,8 +3,8 @@ import boto3
 def detect_text(photo):
 
     client=boto3.client('rekognition',
-                        aws_access_key_id ='AKIAZLP3XMDTUKQAHG7X',
-                        aws_secret_access_key = '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')  # use rekognition
+                        aws_access_key_id ='accessxxxxxxxxxxxxxxxxxxx',
+                        aws_secret_access_key = 'OL/Ixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')  # use rekognition
 
     response=client.detect_text(Image={'Bytes':photo})  #Call API for text_detection
 
